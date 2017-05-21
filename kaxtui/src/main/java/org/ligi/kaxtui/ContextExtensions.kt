205ca0ad50c820ef44e024c0737fd804520bdd19
ligi <ligi@ligi.de>
2017-05-21 12:51:23 +0200

    Add options to set a title for alert() and pass strings as ResourceIds

--- kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
+++ kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
@@ -3,10 +3,21 @@ package org.ligi.kaxtui
 import android.app.AlertDialog
 import android.content.Context
 
+fun Context.alert(message: String, title: String? = null) {
+    val builder = AlertDialog.Builder(this).setMessage(message)
+    title?.let {
+        builder.setTitle(it)
+    }
+    builder.setPositiveButton(android.R.string.ok, null)
+    builder.show()
+}
 
-fun Context.alert(message: String) {
-    AlertDialog.Builder(this).setMessage(message)
-            .setPositiveButton(android.R.string.ok, null)
-            .show()
 
+fun Context.alert(message: Int, title: Int? = null) {
+    val builder = AlertDialog.Builder(this).setMessage(message)
+    title?.let {
+        builder.setTitle(it)
+    }
+    builder.setPositiveButton(android.R.string.ok, null)
+    builder.show()
 }
