9cb338c29eee8074b32529a016ed1a835ca29c0f
ligi <ligi@ligi.de>
2017-07-11 20:15:10 +0200

    Add onOKListener

--- kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
+++ kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
@@ -2,23 +2,24 @@ package org.ligi.kaxtui
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.support.annotation.StringRes
 
-fun Context.alert(message: String, title: String? = null) {
+fun Context.alert(message: String, title: String? = null, onOKListener: DialogInterface.OnClickListener? = null) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, null)
+    builder.setPositiveButton(android.R.string.ok, onOKListener)
     builder.show()
 }
 
 
-fun Context.alert(@StringRes message: Int, @StringRes title: Int? = null) {
+fun Context.alert(@StringRes message: Int, @StringRes title: Int? = null, onOKListener: DialogInterface.OnClickListener? = null) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, null)
+    builder.setPositiveButton(android.R.string.ok, onOKListener)
     builder.show()
 }
