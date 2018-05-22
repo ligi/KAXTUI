fc4624b0d8978baf6f5f86094b3aabaefb4bf823
ligi <ligi@ligi.de>
2018-05-22 17:46:07 +0200

    Improve the listeners - use lambdas

--- kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
+++ kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
@@ -2,24 +2,31 @@ package org.ligi.kaxtui
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.support.annotation.StringRes
 
-fun Context.alert(message: String, title: String? = null, onOKListener: DialogInterface.OnClickListener? = null) {
+fun Context.alert(message: String,
+                  title: String? = null,
+                  onCancel: () -> Unit = {},
+                  onOK: () -> Unit = {}) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, onOKListener)
+    builder.setPositiveButton(android.R.string.ok, { _, _ -> onOK() })
+    builder.setOnCancelListener { onCancel() }
     builder.show()
 }
 
 
-fun Context.alert(@StringRes message: Int, @StringRes title: Int? = null, onOKListener: DialogInterface.OnClickListener? = null) {
+fun Context.alert(@StringRes message: Int,
+                  @StringRes title: Int? = null,
+                  onCancel: () -> Unit = {},
+                  onOK: () -> Unit = {}) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, onOKListener)
+    builder.setPositiveButton(android.R.string.ok, { _, _ -> onOK() })
+    builder.setOnCancelListener { onCancel() }
     builder.show()
 }
