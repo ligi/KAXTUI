47861a3bbc05160ed63fd566870d27d1583db8de
ligi <ligi@ligi.de>
2018-11-28 20:09:46 +0100

    If onCancel is not given -> use onOK

--- kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
+++ kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
@@ -1,32 +1,34 @@
 package org.ligi.kaxtui
 
+import android.app.AlertDialog
 import android.content.Context
 import android.support.annotation.StringRes
-import android.support.v7.app.AlertDialog
 
 fun Context.alert(message: String,
                   title: String? = null,
-                  onCancel: () -> Unit = {},
+                  onCancel: (() -> Unit)? = null,
                   onOK: () -> Unit = {}) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, { _, _ -> onOK() })
-    builder.setOnCancelListener { onCancel() }
+    builder.setPositiveButton(android.R.string.ok) { _, _ -> onOK() }
+    builder.setOnCancelListener { if (onCancel != null) onCancel() else onOK() }
     builder.show()
 }
 
 
 fun Context.alert(@StringRes message: Int,
                   @StringRes title: Int? = null,
-                  onCancel: () -> Unit = {},
+                  onCancel: (() -> Unit)? = {},
                   onOK: () -> Unit = {}) {
     val builder = AlertDialog.Builder(this).setMessage(message)
     title?.let {
         builder.setTitle(it)
     }
-    builder.setPositiveButton(android.R.string.ok, { _, _ -> onOK() })
-    builder.setOnCancelListener { onCancel() }
+    builder.setPositiveButton(android.R.string.ok) { _, _ -> onOK() }
+    builder.setOnCancelListener {
+        if (onCancel != null) onCancel() else onOK()
+    }
     builder.show()
 }
