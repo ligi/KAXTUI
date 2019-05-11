3e029bc4df4d152cac1b982d731eaef48477bab0
ligi <ligi@ligi.de>
2019-05-11 22:18:40 +0200

    Migrate to AndroidX

--- kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
+++ kaxtui/src/main/java/org/ligi/kaxtui/ContextExtensions.kt
@@ -1,8 +1,8 @@
 package org.ligi.kaxtui
 
 import android.content.Context
-import android.support.annotation.StringRes
-import android.support.v7.app.AlertDialog
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 
 fun Context.alert(message: String,
                   title: String? = null,
